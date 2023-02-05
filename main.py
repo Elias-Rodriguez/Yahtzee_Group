bddfe9758060df6a541aa6cc8cbc40737cc4f27f
Cthax12 <Cthax12@gmail.com>
2023-02-04 18:07:57 -0600

    updated main

--- main.py
+++ main.py
@@ -7,27 +7,14 @@ import Faff_file
 from Player import Player
 from Dice import Dice
 
-#our_object = Faff_file.User_interactions()
+our_object = Faff_file.User_interactions()
+our_object.ask_player_count()
+our_object.ask_player_names()
+keepPlaying = True
+while keepPlaying:
+    for player_number in range(0, our_object.get_player_count()):
+        print(f'ready player {player_number + 1}, aka {our_object.get_player_name(player_number)}')
 
-#our_object.ask_player_count()
-
-#our_object.ask_player_names()
-
-#keepPlaying = True
-
-#while keepPlaying:
-#    for player_number in range(0, our_object.get_player_count()):
-#        print(f'ready player {player_number + 1}, aka {our_object.get_player_name(player_number)}')
-#
-#        our_object.roll_new_five()
-#        our_object.ask_player_what_to_keep()
-#    print('good round everyone!')
-#
-
-player_count = int(input("How many players would you like?"))
-
-
-for i in range(0, player_count):
-
-    dice = Dice()
-    dice.first_roll()
+        our_object.roll_new_five()
+        our_object.ask_player_what_to_keep()
+    print('good round everyone!')
